10e0ef07b74369c9eec7d22d801ce9a6bbcbf57a
Nick Stepanov <nikstpnov@gmail.com>
2020-11-10 13:42:19 +0300

    Add some mixins and variables

--- src/components/search.js
+++ src/components/search.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import SearchIcon from './../images/search.png';
 
 const Search = () => {
+    const searchIconStyle = `url(${SearchIcon})`;
+
     return (
         <form className="search" onSubmit={ (e) => {e.preventDefault()} }>
+            <div className="search-icon" style={{backgroundImage: searchIconStyle, backgroundSize: 'contain'}}></div>
             <input className="input" type="text"/>
         </form>
     )
