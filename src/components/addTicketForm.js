b478601cdd33ad38c27a586b3bbcc15fd302bb7e
Nick Stepanov <nikstpnov@gmail.com>
2020-11-14 15:51:31 +0300

    Finish 'add ticket' function

--- src/components/addTicketForm.js
+++ src/components/addTicketForm.js
@@ -2,36 +2,68 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { setTickets } from './../actions';
 
+import KentPhoto from './../images/kent.jpeg';
+import WaynePhoto from './../images/wayne.jpg';
+import ParkerPhoto from './../images/parker.jpg';
+
 
 const AddTicketForm = () => {
     const ticketsArr = useSelector(state => state.tickets);
 
     const dispatch = useDispatch();
 
+    const [wayne, kent, parker] = [{ name: "Bruce Wayne", photo: WaynePhoto, profession: "Billionaire"},
+                                { name: "Clark Kent", photo: KentPhoto, profession: "Electrician"},
+                                { name: "Peter Parker", photo: ParkerPhoto, profession: "Student"}];
+
     const addNewTicket = (e) => {
         e.preventDefault();
-        const dateTime = `${now.getDate()}/${now.getMonth() + 1}/${now.getFullYear()} ${now.getHours()}:${now.getMinutes()}`
+        const now = new Date();
+        const dateTime = `${now.getDate()}/${now.getMonth() + 1}/${now.getFullYear()} ${now.getHours()}:${now.getMinutes()}`;
         const form = e.target;
-        const newTicket = {owner: { name: wayne.name, photo: wayne.photo, profession: wayne.profession },
-                            details: { reported: dateTime, status: "ASD", description: form.inputDescription.value },
-                            asset: { name: form.assetName.value, geoCode: form.inputGeoCode.value, location: { x: form.inputlocationX, y: form.inputlocationY } },
+        let owner = {};
+        let status = '';
+        if (form.owner.value === 'wayne') {
+            owner = wayne;
+        } else if (form.owner.value === 'kent') {
+            owner = kent;
+        } else {
+            owner = parker;
+        };
+        const newTicket = {owner,
+                            details: { reported: dateTime, status: form.status.value, description: form.inputDescription.value },
+                            asset: { name: form.inputAssetName.value, geoCode: form.inputGeoCode.value, location: { x: form.inputLocationX.value, y: form.inputLocationY.value } },
                             choosen: false, id: Math.random()};
         dispatch(setTickets([...ticketsArr, newTicket]));
+    }
+
+    const clearForm = (e) => {
+        e.preventDefault();
+        const form = e.target;
+        form.owner.value = 'wayne';
+        form.status.value = 'UNA';
+        form.inputDescription.value = '';
+        form.inputAssetName.value = '';
+        form.inputGeoCode.value = '';
+        form.inputLocationX.value = '';
+        form.inputLocationY.value = '';
+
+
     }
 
 
     return (
         <div className="add-form-wrapper">
             <div className="title_addTicket title">Add Ticket</div>
-            <form className="add-form" onSubmit={ (e) => addNewTicket(e) }>
+            <form className="add-form" onSubmit={ (e) => { addNewTicket(e); clearForm(e) } }>
                 <label className="label">Choose a hero</label>
-                <select className="input">
+                <select id="owner" className="input">
                     <option value="wayne">Bruce Wayne</option>
                     <option value="kent">Clark Kent</option>
                     <option value="parker">Peter Parker</option>
                 </select>
                 <label className="label">Choose status</label>
-                <select id="" className="input">
+                <select id="status" className="input">
                     <option value="UNA">UNA</option>
                     <option value="ASD">ASD</option>
                     <option value="COM">COM</option>
@@ -40,10 +72,10 @@ const AddTicketForm = () => {
                 <textarea id="inputDescription" className="input textarea" cols="30" rows="10" placeholder="Enter description of ticket"></textarea>
                 <label htmlFor="inputAssetName" className="label">Asset name</label>
                 <input id="inputAssetName" className="input" type="text" placeholder="Enter asset name" />
-                <label htmlFor="inputGeocode" className="label">GeoCode</label>
-                <input id="inputGeocode" className="input" type="text" placeholder="Enter GeoCode" />
+                <label htmlFor="inputGeoCode" className="label">GeoCode</label>
+                <input id="inputGeoCode" className="input" type="text" placeholder="Enter GeoCode" />
                 <label className="label">Location</label>
-                <div className="location-wrapper">
+                <div id="location" className="location-wrapper">
                     <input id="inputLocationX" className="input" type="text" placeholder="X" />
                     <input id="inputLocationY" className="input" type="text" placeholder="Y" />
                 </div>
